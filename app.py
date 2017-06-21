3eafbd131b932315be0fb22368b5c0c51779732f
py-chemist <py.chemist@gmail.com>
2017-06-21 17:42:58 -0400

    Create flask boilerplate

---
+++ app.py
@@ -0,0 +1,12 @@
+from Flask import flask, render_template, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
